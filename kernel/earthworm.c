84e427700fc05d0cd036b468ebfd7b27f2d7a363
root <root@tpx64.int.sedamit.hr>
2013-02-21 14:08:16 +0100

    removed obsolete code

--- kernel/earthworm.c
+++ kernel/earthworm.c
@@ -287,50 +287,6 @@ find_sys_call_table(void)
 {
 	return (void *)SYS_CALL_ADDRESS;
 }
-
-/* obsolete 
-static void * 
-__deprecated find_sys_call_table(void)
-{
-        struct {
-                unsigned short  limit;
-                unsigned int    base;
-        } __attribute__ ( ( packed ) ) idtr;
-
-        struct {
-                unsigned short  offset_low;
-                unsigned short  segment_select;
-                unsigned char   reserved,   flags;
-                unsigned short  offset_high;
-        } __attribute__ ( ( packed ) ) * idt;
-
-        unsigned long system_call = 0;        // x80ä¸­æ­å¤çç¨åºsystem_call å°å
-        char *call_hex = "\xff\x14\x85";        // call æä»¤
-        char *code_ptr = NULL;
-        char *p = NULL;
-        unsigned long sct = 0x0;
-        int i = 0;
-
-        __asm__ ( "sidt %0": "=m" ( idtr ) );
-        idt = ( void * ) ( idtr.base + 8 * 0x80 );
-        system_call = ( idt->offset_high << 16 ) | idt->offset_low;
-
-        code_ptr = (char *)system_call;
-        for(i = 0;i < ( 100 - 2 ); i++) {
-        	if(code_ptr[i] == call_hex[0] && 
-						 code_ptr[i+1] == call_hex[1] && 
-						 code_ptr[i+2] == call_hex[2] )  {
-						p = &code_ptr[i] + 3;
-            break;
-					}
-        }
-
-        if ( p ){
-					sct = *(unsigned long*)p;
-        }
-        return (unsigned long*)sct;
-}
-*/
 #endif
 
 int make_rw(unsigned long address)
